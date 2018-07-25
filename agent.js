c87c52c85a3409f66f9a39f04bfbbfee1cc34de1
relzhong <relzhong@outlook.com>
2018-07-25 14:03:43 +0800

    fix bugs

--- agent.js
+++ agent.js
@@ -1,5 +1,5 @@
 'use strict';
 
 module.exports = app => {
-  if (app.config.multiCache.agent) require('./lib/cacheLoader')(app);
+  if (app.config.multiCache.agent) require('./lib/cache-loader')(app);
 };
